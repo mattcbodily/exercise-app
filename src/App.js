3e5d529e2b85baa730b072f27bf6f62d6fe388fe
mattcbodily <mattcbodily@gmail.com>
2020-01-03 15:40:38 -0700

    created landing styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Landing from './Components/Landing/Landing';
 import './reset.scss';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Landing />
     </div>
   );
 }
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,2 +1,16 @@
 import React, {useState} from 'react';
+import './Landing.scss';
+import bike from '../../assets/road-bike.png';
 
+const Landing = () => {
+    return (
+        <div class='landing'>
+            <img src={bike} alt='road-bike' class='modal-image'/>
+            <input />
+            <br/>
+            <input type='password'/>
+        </div>
+    )
+}
+
+export default Landing;
