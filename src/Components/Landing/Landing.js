0bd78fbad697e67fa629b77a9973fbc3d843499f
mattcbodily <mattcbodily@gmail.com>
2020-01-06 23:14:00 -0700

    added conditional rendering for register

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,13 +3,35 @@ import './Landing.scss';
 import bike from '../../assets/road-bike.png';
 
 const Landing = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [verPassword, setVerPassword] = useState('');
+    const [showRegister, setShowRegister] = useState(false)
+
+    const toggleRegister = () => {
+        setShowRegister(!showRegister)
+    }
+
     return (
         <div class='landing'>
             <img src={bike} alt='road-bike' class='modal-image'/>
             <h1>AppName Here</h1>
-            <input />
+            <input value={email}/>
             <br/>
-            <input type='password'/>
+            <input type='password' value={password}/>
+            {!showRegister
+            ? (
+                <>
+                    <button onClick={toggleRegister}>Login</button>
+                    <p>Register Here</p>
+                </>
+            ) : (
+                <>
+                    <input type='password' value={verPassword}/>
+                    <button onClick={toggleRegister}>Register</button>
+                    <p>Login Here</p>
+                </>
+            )}
             <h3>Exercise Gamified.</h3>
         </div>
     )
