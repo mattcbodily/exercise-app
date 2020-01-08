3f54aef379ae773f06853ab09bb2c416f1bde9ce
mattcbodily <mattcbodily@gmail.com>
2020-01-08 09:36:02 -0700

    finished auth handler functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,24 +7,44 @@ module.exports = {
               db = req.app.get('db');
         
         let user = await db.auth.check_user({email});
-        if(user){
+        if(user[0]){
             return res.status(400).send('Email already in use')
         }
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
 
         const createdUser = await db.auth.register_user({email, password: hash});
-        session.user = createdUser;
+        session.user = createdUser[0];
         res.status(201).send(session.user);
     },
     login: (req, res) => {
+        const {email, password} = req.body,
+        {session} = req,
+        db = req.app.get('db');
 
+        let user = db.auth.check_user({email});
+        if(!user){
+            res.status(400).send('Email is incorrect');
+        }
+
+        const authorized = bcrypt.compareSync(password, user[0].password);
+        if(!authorized){
+            res.status(400).send('Password is incorrect');
+        }
+
+        delete user[0].password
+        session.user = user[0];
+        res.status(202).send(session.user);
     },
     logout: (req, res) => {
         req.session.destroy();
         res.sendStatus(200);
     },
     getUser: (req, res) => {
-
+        if(req.session.user){
+            res.status(200).send(req.session.user)
+        } else {
+            res.status(200).send('No user logged in')
+        }
     }
 }
